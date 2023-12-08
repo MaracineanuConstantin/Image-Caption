57f6a5b9b24581b8e692e132c11685a7e485a17e
Maracineanu Constantin <maracineanu.constantin@gmail.com>
2023-12-09 01:58:45 +0200

    added a training_log file for the train and implemented a model saving based on model best_loss, also added some parameters for future model validation

--- train.py
+++ train.py
@@ -3,18 +3,24 @@ import torch
 import torch.nn as nn
 import numpy as np
 import os
+import sys
 import pickle
 from data_loader import get_loader 
 from build_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
+# from anothermodel import EncoderCNN, DecoderRNN
 from torch.nn.utils.rnn import pack_padded_sequence
 from torchvision import transforms
+import time
 
+best_loss = 100
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def main(args):
+    global best_loss
+
     # Create model directory
     if not os.path.exists(args.model_path):
         os.makedirs(args.model_path)
@@ -32,9 +38,12 @@ def main(args):
         vocab = pickle.load(f)
     
     # Build data loader
-    data_loader = get_loader(args.image_dir, args.caption_path, vocab, 
+    data_loader = get_loader(args.train_dir, args.caption_path, vocab, 
                              transform, args.batch_size,
                              shuffle=True, num_workers=args.num_workers) 
+
+    # val_loader = val_loader(args.val_dir, args.val_caption_path, vocab, args.batch_size,
+    #                         num_workers=args.num_workers, transform=transform)
 
     # Build the models
     encoder = EncoderCNN(args.embed_size).to(device)
@@ -45,9 +54,15 @@ def main(args):
     params = list(decoder.parameters()) + list(encoder.linear.parameters()) + list(encoder.bn.parameters())
     optimizer = torch.optim.Adam(params, lr=args.learning_rate)
     
+    # Open the training log file
+    f = open(args.log_file, 'w')
+
+    # Time per epoch
+    epoch_time = time.time()
     # Train the models
     total_step = len(data_loader)
     for epoch in range(args.num_epochs):
+        start = time.time()
         for i, (images, captions, lengths) in enumerate(data_loader):
             
             # Set mini-batch dataset
@@ -64,35 +79,54 @@ def main(args):
             loss.backward()
             optimizer.step()
 
-            # Print log info
-            if i % args.log_step == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
-                      .format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item()))) 
-                
+            # Get training statistics.
+            # stats = 'Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Perplexity: %5.4f' % (epoch, num_epochs, i_step, total_step, loss.item(), np.exp(loss.item()))
+
+            end = time.time()
+
+            stats = 'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'.format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item()))
+
+            # Print training statistics .
+            if (i+1) % args.log_step == 0:
+                print(stats)
+                f.write(stats + '\n')
+                f.flush()
+            
             # Save the model checkpoints
-            if (i+1) % args.save_step == 0:
+            if loss.item() < best_loss:
+                best_loss = loss.item()
                 torch.save(decoder.state_dict(), os.path.join(
                     args.model_path, 'decoder-{}-{}.ckpt'.format(epoch+1, i+1)))
                 torch.save(encoder.state_dict(), os.path.join(
                     args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
-
+            
+        end = time.time()
+        end_time = end - start
+        f.write(f"Time on {epoch} epoch is: {end_time} + '\n")
+        f.flush()
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, default='models/' , help='path for saving trained models')
     parser.add_argument('--crop_size', type=int, default=224 , help='size for randomly cropping images')
     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
-    parser.add_argument('--image_dir', type=str, default='data/resized2014', help='directory for resized images')
+    parser.add_argument('--train_dir', type=str, default='data/resized2014', help='directory for resized train images')
+    parser.add_argument('--val_dir', type=str, default='data/resizedval2014', help='directory for resized val images')
     parser.add_argument('--caption_path', type=str, default='data/annotations/captions_train2014.json', help='path for train annotation json file')
-    parser.add_argument('--log_step', type=int , default=10, help='step size for prining log info')
+    parser.add_argument('--val_caption_path', type=str, default='data/annotations/captions_val2014.json', help='path for val annotation json file')
+    parser.add_argument('--log_step', type=int , default=100, help='step size for prining log info')
     parser.add_argument('--save_step', type=int , default=1000, help='step size for saving trained models')
     
+    # log file
+    parser.add_argument('--log_file', type=str, default='training_log.txt', help='name of the log file + .txt')
+
     # Model parameters
     parser.add_argument('--embed_size', type=int , default=256, help='dimension of word embedding vectors')
     parser.add_argument('--hidden_size', type=int , default=512, help='dimension of lstm hidden states')
     parser.add_argument('--num_layers', type=int , default=1, help='number of layers in lstm')
     
-    parser.add_argument('--num_epochs', type=int, default=5)
+    parser.add_argument('--num_epochs', type=int, default=30)
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--learning_rate', type=float, default=0.001)
