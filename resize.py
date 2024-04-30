1057e3eab83c76a74899457527f19c246cbeb51a
Maracineanu Constantin <maracineanu.constantin@gmail.com>
2024-04-30 22:17:37 +0300

    update resize values

--- resize.py
+++ resize.py
@@ -32,11 +32,11 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_dir', type=str, default='./data/train2017/',
+    parser.add_argument('--image_dir', type=str, default='./data/val2017/',
                         help='directory for train images')
-    parser.add_argument('--output_dir', type=str, default='./data/resizedtrain2017/',
+    parser.add_argument('--output_dir', type=str, default='./data/resized192val2017/',
                         help='directory for saving resized images')
-    parser.add_argument('--image_size', type=int, default=256,
+    parser.add_argument('--image_size', type=int, default=192,
                         help='size for image after processing')
     args = parser.parse_args()
     main(args)
